2c759cacad72a12b76a87a5a72ab22f95a273a94
manavkdubey <dmanavkumar24@gmail.com>
2025-10-27 20:54:25 +0530

    Initial commit with daily workflow

---
+++ q1.js
@@ -0,0 +1,5 @@
+// Hacker News RSS API - Latest AI post with minimum 89 points
+
+const latestAIPostURL = "https://jacobin.com/2025/10/ice-zignal-surveillance-social-media";
+
+console.log(latestAIPostURL);
